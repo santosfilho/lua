cdbfcfc4e20fc8e38b30ee14f8fecf0fb3dea0ad
José dos Santos <josefilho@info.ufrn.br>
2019-07-17 16:37:32 -0300

    Base spring boot

---
+++ src/main/java/com/automacao/lua/LuaApplication.java
@@ -0,0 +1,13 @@
+package com.automacao.lua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuaApplication.class, args);
+	}
+
+}
